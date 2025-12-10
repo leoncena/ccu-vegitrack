3ab54c1946d98bee26815abc451ccee85320b11f
Leon Cena <leon.cena@tum.de>
2025-12-10 12:24:41 +0000

    css change

--- vegitrack/src/components/ui/combobox.tsx
+++ vegitrack/src/components/ui/combobox.tsx
@@ -104,7 +104,7 @@ export function Combobox({
         style={{ width: popoverWidth ? `${popoverWidth}px` : undefined }}
       >
         <Command>
-          <CommandInput placeholder={searchPlaceholder} />
+          <CommandInput placeholder={searchPlaceholder} autoFocus={false} />
           <CommandList>
             <CommandEmpty>{emptyText}</CommandEmpty>
             <CommandGroup>
